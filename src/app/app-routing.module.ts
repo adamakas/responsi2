35fb107665b43a765b35cb46f4656a98f3c39461
adamakas <adamakas18@gmail.com>
2022-12-06 20:20:39 +0700

    Responsi 2_H1D020037_Adam Rayhan Akas Nurfaza

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,41 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
+import { AutoLoginGuard } from './guards/auto-login.guard';
+import { AuthGuard } from './guards/auth.guard';
 const routes: Routes = [
+ {
+ path: 'home',
+ loadChildren: () => import('./home/home.module').then(m => m.HomePageModule),
+ canLoad: [AuthGuard] // Secure all child pages
+ },
+ {
+ path: '',
+ redirectTo: 'login',
+ pathMatch: 'full'
+ },
+ {
+ path: 'login',
+ loadChildren: () => import('./login/login.module').then(m =>
+ m.LoginPageModule),
+ canLoad: [AutoLoginGuard]
+ },
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: 'matakuliah',
+    loadChildren: () => import('./matakuliah/matakuliah.module').then( m => m.MatakuliahPageModule)
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'matakuliah-tambah',
+    loadChildren: () => import('./matakuliah-tambah/matakuliah-tambah.module').then( m => m.MatakuliahTambahPageModule)
+  },
+  {
+    path: 'matakuliah-edit/:id',
+    loadChildren: () => import('./matakuliah-edit/matakuliah-edit.module').then( m => m.MatakuliahEditPageModule)
   },
 ];
-
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+ imports: [
+ RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+ ],
+ exports: [RouterModule]
 })
 export class AppRoutingModule { }
